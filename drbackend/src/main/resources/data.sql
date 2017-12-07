aa190fe1c8c1d68588e2d27c005e84a8af0da970
{Danniel.c} <{Dannielchung@gmail.com}>
2017-12-08 07:36:15 +0800

    add default user data sql for jwt

---
+++ drbackend/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO users( firstname, lastname, password, username) VALUES ('Chiang', 'Danniel', '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', 'admin');
+INSERT INTO users( firstname, lastname, password, username) VALUES ('Lin', 'Yelena', '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', 'user');
+INSERT INTO AUTHORITY (name) VALUES ('ROLE_USER');
+INSERT INTO AUTHORITY (name) VALUES ('ROLE_ADMIN');
+INSERT INTO USER_AUTHORITY (user_id, authority_id) VALUES (1, 1);
+INSERT INTO USER_AUTHORITY (user_id, authority_id) VALUES (2, 1);
+INSERT INTO USER_AUTHORITY (user_id, authority_id) VALUES (2, 2);
